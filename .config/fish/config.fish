7fac898935fbb2267ce821a4bb384b0721f9e17d
Pavel Gabriel <alovak@gmail.com>
2024-02-21 11:30:51 +0100

    add fish functions

---
+++ .config/fish/config.fish
@@ -0,0 +1,54 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -U fish_greeting "🐟"
+    rbenv init - fish | source
+end
+
+function vi
+	switch (uname)
+		case Linux
+			vim $argv
+		case Darwin
+			mvim -v $argv
+	end
+end
+
+function vim
+	switch (uname)
+		case Linux
+			vim $argv
+		case Darwin
+			mvim -v $argv
+	end
+end
+
+function gs
+    git status $argv[1]
+end
+
+function ts --wraps tmux
+    tmux new-session -A -s $argv[1]
+end
+
+
+function drestart
+    docker-compose up -d --force-recreate --no-deps $argv
+end
+
+function gots
+    gotestsum -- $argv
+end
+
+function gocover
+    go test -coverprofile=coverage.out
+    go tool cover -html=coverage.out
+end
+
+function gocoverall
+    go test ./... -coverprofile=coverage.out
+    go tool cover -html=coverage.out
+end
+
+function config
+    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+end
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,90 @@
+function fish_prompt --description 'Informative prompt'
+    #Save the return status of the previous command
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+    if not set -q __fish_git_prompt_show_informative_status
+        set -g __fish_git_prompt_show_informative_status 1
+    end
+    if not set -q __fish_git_prompt_hide_untrackedfiles
+        set -g __fish_git_prompt_hide_untrackedfiles 1
+    end
+    if not set -q __fish_git_prompt_color_branch
+        set -g __fish_git_prompt_color_branch magenta --bold
+    end
+    if not set -q __fish_git_prompt_showupstream
+        set -g __fish_git_prompt_showupstream informative
+    end
+    if not set -q __fish_git_prompt_char_upstream_ahead
+        set -g __fish_git_prompt_char_upstream_ahead "↑"
+    end
+    if not set -q __fish_git_prompt_char_upstream_behind
+        set -g __fish_git_prompt_char_upstream_behind "↓"
+    end
+    if not set -q __fish_git_prompt_char_upstream_prefix
+        set -g __fish_git_prompt_char_upstream_prefix ""
+    end
+    if not set -q __fish_git_prompt_char_stagedstate
+        set -g __fish_git_prompt_char_stagedstate "●"
+    end
+    if not set -q __fish_git_prompt_char_dirtystate
+        set -g __fish_git_prompt_char_dirtystate "✚"
+    end
+    if not set -q __fish_git_prompt_char_untrackedfiles
+        set -g __fish_git_prompt_char_untrackedfiles "…"
+    end
+    if not set -q __fish_git_prompt_char_invalidstate
+        set -g __fish_git_prompt_char_invalidstate "✖"
+    end
+    if not set -q __fish_git_prompt_char_cleanstate
+        set -g __fish_git_prompt_char_cleanstate "✔"
+    end
+    if not set -q __fish_git_prompt_color_dirtystate
+        set -g __fish_git_prompt_color_dirtystate blue
+    end
+    if not set -q __fish_git_prompt_color_stagedstate
+        set -g __fish_git_prompt_color_stagedstate yellow
+    end
+    if not set -q __fish_git_prompt_color_invalidstate
+        set -g __fish_git_prompt_color_invalidstate red
+    end
+    if not set -q __fish_git_prompt_color_untrackedfiles
+        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    end
+    if not set -q __fish_git_prompt_color_cleanstate
+        set -g __fish_git_prompt_color_cleanstate green --bold
+    end
+
+    if functions -q fish_is_root_user; and fish_is_root_user
+        printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
+                                                         and set_color $fish_color_cwd_root
+                                                         or set_color $fish_color_cwd) \
+            (prompt_pwd) (set_color normal)
+    else
+        set -l status_color (set_color $fish_color_status)
+        set -l statusb_color (set_color --bold $fish_color_status)
+        set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+	set -l jobs_color (set_color E5C07B)
+	set -l num_jobs (count (jobs -p))
+
+        printf '%s%s@%s %s%s %s%s%s' (set_color brblue) \
+            $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
+            (set_color normal)
+
+	fish_vcs_prompt '\n%s'
+
+        echo
+
+	# Display the number of jobs if more than 0
+	if test $num_jobs -gt 0
+		set_color --bold E5C07B;
+		echo -n "[$num_jobs]"
+		set_color normal;
+		echo -n " "
+	end
+
+	echo -n '➜ '
+
+	# printf "%s" (fish_vcs_prompt) ' $ '
+    end
+end
